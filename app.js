b036f5e326824f737829f19e970b42b8e96a7ac9
yanntyb <69966391+yanntyb@users.noreply.github.com>
2020-12-01 16:50:53 +0100

    a

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log(random());
+let operande = 10;
+
+let entier = document.getElementById("entier");
+let flottant = document.getElementById("flottant");
+let nonnumerique = document.getElementById("non-numerique");
+let trunc = document.getElementById("trunc");
+let phrase = document.getElementById("replace");
+
+entier.innerHTML *= operande;
+flottant.innerHTML = parseFloat((operande * flottant.innerHTML) + parseInt(entier.innerHTML));
+
+if(isNaN(nonnumerique.innerHTML)){
+    nonnumerique.innerHTML = 0
+}
+
+document.getElementById("random").innerHTML = Math.random()*100
+trunc.innerHTML = Math.trunc(trunc.innerHTML)
+phrase.innerHTML = phrase.innerHTML.replace("world","yann tyburczy")
